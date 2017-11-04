00996b12590cb4efdb010a53a3d312e7325e6501
howmuchadollarcost <muluyonas@yahoo.com>
2017-11-04 17:51:31 +0100

    check ball_cml

--- ball_cml_tcheck.py
+++ ball_cml_tcheck.py
@@ -1,12 +1,20 @@
 """
-4.9
-def ball_pos(t,v0,g=9.81):
+import sys
+g = 9.81
+def ball_pos(t,v0):
     return v0*t - 0.5*g*t**2
 t = float(input("Please Enter time(s): " ))
 v0 = float(input("Please Enter initial velocity(m/s): "))
-y = ball_pos(t,v0)
-print("The position (y) after %.2f s and %.2f m/s is %.2f m" %(t,v0,y))
+
+
+if 0 <= t <= (2*v0)/(g):
+    y = ball_pos(t,v0)
+    print("The position (y) after %.2f s and %.2f m/s is %.2f m" %(t,v0,y))
+else:
+    print("Time(t) is should be between 0 and %.3f" %(2*v0/g))
+    sys.exit(1)
 """
+g  = 9.81
 import sys
 
 def ball_pos(t,v0,g=9.81):
@@ -27,5 +35,10 @@ except ValueError:
     print("Please give the values in right float form not %s and %s" %(sys.argv[1],sys.argv[2]))
     sys.exit(1)
 
-y = ball_pos(t,v0)
-print("The position (y) after %.2f s and %.2f m/s is %.2f m" %(t,v0,y))
+if 0 <= t <= (2*v0)/(g):
+    y = ball_pos(t,v0)
+    print("The position (y) after %.2f s and %.2f m/s is %.2f m" %(t,v0,y))
+else:
+    print("Time(t) is should be between 0 and %.3f" %(2*v0/g))
+    sys.exit(1)
+ 
